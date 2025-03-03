25eca5b32053f552b701155b6a6f7dec7958b581
Guillaume Scigala <guillaume@scigala.fr>
2025-03-03 18:09:04 +0100

    Update to version v0.6.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='communator-vigicrues',
-    version='0.5.0',
+    version='0.6.0',
     packages=find_packages(where='.'),
     package_dir={'': '.'},
     install_requires=[
